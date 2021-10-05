12ffaf934eb0b3b827fa9bb33346198b6037278d
Zulbil <alexkhang25@yahoo.fr>
2021-10-05 11:26:09 +0100

    correction in modal message

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+import PropTypes from 'prop-types'
 
-const Modal = ({ success, amount }) => {
-    return (
-        <div>
-            <p>NOTIFICATION ALERT</p>
-        </div>
-    )
+
+const CustomModal = ({ display, message }) => {
+
+    const btnEl             =   useRef(null) 
+    const [show, setShow]   =   useState(false) 
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const triggerClick = () => {
+        if(display) {
+            btnEl.current.click()
+        }
+    }
+
+    useEffect(() => {
+        triggerClick()
+    }, [display])
+
+  return (
+    <>
+        <Button ref={btnEl} className="d-none" variant="primary" onClick={handleShow}>
+            Launch demo modal
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </>
+  );
 }
 
 Modal.propTypes = {
-    success: PropTypes.Boolean,
-    amount: PropTypes.string
+    display: PropTypes.bool,
+    message: PropTypes.string
 }
 
-export default Modal
+export default CustomModal
